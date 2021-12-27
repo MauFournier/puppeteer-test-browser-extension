0c4281209178e515d871fdb9ead0bb3afb4e20b2
Mau Fournier <mau.fournier@gmail.com>
2021-12-27 10:51:40 -0600

    feat: Support both ESM and CJS

---
+++ lib/esm/puppeteer-test-browser-extension.d.ts
@@ -0,0 +1,14 @@
+import puppeteer from 'puppeteer';
+interface IBootstrapOptions {
+    devtools?: boolean;
+    slowMo?: number;
+    contentUrl: string;
+    pathToExtension: string;
+}
+declare const bootstrapExtension: (options: IBootstrapOptions) => Promise<{
+    contentPage: puppeteer.Page;
+    browser: puppeteer.Browser;
+    extensionUrl: string;
+    extensionPage: puppeteer.Page;
+}>;
+export default bootstrapExtension;
